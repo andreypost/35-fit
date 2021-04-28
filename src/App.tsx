d4100ed9e4b97d8486bfcb1681b49fabcbf5377b
andrey post <postoliuk.av@gmail.com>
2021-04-28 11:33:09 +0300

    set english in the end

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,5 @@
 import React, { useEffect, useState } from "react";
+import empty from './img/empty_img.png'
 // import png from './img/header_main.png'
 // import svg from './img/inlineSvg/main_header_back.svg'
 // import { Buttom } from './components/Button'
@@ -20,7 +21,7 @@ export const App = (): any => {
     //     return a + b + c
     // }
     const [title, setTitle] = useState({ src: '', word: '', lang: '' }),
-        [initialValue, setInitialValue] = useState(''),
+        [initial, setInitial] = useState({ value: '', code: '' }),
         [pointerEvents, setPointerEvents] = useState(''),
         languages = [{ 'code': 'fr', 'lang': 'French' },
         { 'code': 'it', 'lang': 'Italian' },
@@ -31,12 +32,11 @@ export const App = (): any => {
         { 'code': 'ar', 'lang': 'Arabic' },
         { 'code': 'zh', 'lang': 'Chinese' }],
         languageSet: Set<{ code: string; lang: string; }> = new Set(),
-        translationSet: Set<{ word: any; lang: string; }> = new Set([{ word: initialValue, lang: 'English' }])
+        translationSet: Set<{ word: any; lang: string; }> = new Set([{ word: initial.value, lang: 'English' }])
 
-    let initialCode = 'en'
-
-    const getImages = async (wordResults: Set<{ word: any; lang: string; }>) => {
-        for await (const item of wordResults) {
+    const getImages = async (translationResults: Set<{ word: any; lang: string; }>) => {
+        console.log(translationResults)
+        for await (const item of translationResults) {
             fetch(`https://api.pexels.com/v1/search?query=${item.word}&per_page=1`, {
                 "headers": {
                     "Authorization": "563492ad6f91700001000001173c2e1310614e4c9a6b3f0fe56afc68",
@@ -45,47 +45,44 @@ export const App = (): any => {
                 }
             })
                 .then(response => response.json())
-                .then(items => {
-                    setTitle({ src: items.photos[0].src.medium, word: item.word, lang: item.lang })
-                })
-                .catch(err => console.log(err))
+                .then(items => setTitle({ src: items.photos[0].src.medium, word: item.word, lang: item.lang }))
+                .catch(() => setTitle({ src: empty, word: item.word, lang: item.lang }))
             await new Promise(resolve => setTimeout(resolve, 2000))
         }
-        setPointerEvents('')
     }
 
-    const getTranslation = async () => {
+    const getTranslation = async (languageSet: Set<{ code: string; lang: string; }>) => {
         for await (const item of languageSet) {
-            await fetch(`https://api.mymemory.translated.net/get?q=${initialValue}&langpair=${initialCode}|${item.code}`)
+            await fetch(`https://api.mymemory.translated.net/get?q=${initial.value}&langpair=${initial.code}|${item.code}`)
                 .then(response => response.json())
                 .then(result => {
-                    setInitialValue(result.responseData.translatedText)
+                    setInitial({ value: result.responseData.translatedText, code: item.code })
                     translationSet.add({ word: result.responseData.translatedText, lang: item.lang })
-                    initialCode = item.code
                 })
-                .catch(() => getTranslation())
+                .catch(() => getTranslation(languageSet))
         }
-        // const results = await Promise.all(wordSet)
-        console.log(translationSet)
+        // // const results = await Promise.all(wordSet)
         getImages(translationSet)
-        setInitialValue('')
+            .then(() => {
+                setInitial({ value: '', code: '' })
+                setPointerEvents('')
+            })
     }
 
     const haldleSubmit = (e: { preventDefault: () => void; }) => {
         e.preventDefault()
         setPointerEvents('active')
-        while (languageSet.size < languages.length - 1) {
+        while (languageSet.size < languages.length) {
             languageSet.add(languages[Math.floor(Math.random() * languages.length)])
         }
-        getTranslation()
+        languageSet.add({ 'code': 'es', 'lang': 'English' })  // lang is 'Spanish', this API does not allow to put two the same languages at a time.
+        getTranslation(languageSet)
     }
-
-
 
     return (
         <>
             <form action="get" id="wordForm" onSubmit={haldleSubmit}>
-                <input type="text" name="initial" pattern="^[a-zA-Z]+$" required value={initialValue} onChange={e => setInitialValue(e.target.value)} />
+                <input type="text" name="initial" pattern="^[a-zA-Z]+$" required value={initial.value} onChange={e => setInitial({ value: e.target.value, code: 'en' })} />
                 <button type="submit" className={pointerEvents}>submit</button>
             </form>
             <div>
