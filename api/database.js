4e78e64f18a8f05da15d502312f12425acb8af59
andrii post <postoliuk.av@gmail.com>
2024-05-22 17:18:52 +0300

    added dotenv; eddited readme

--- api/database.js
+++ api/database.js
@@ -1,14 +1,19 @@
 const { Sequelize } = require("sequelize");
+require("dotenv").config();
 
 const sequelize = new Sequelize(
-  "fitdb",
+  process.env.DB_NAME,
   process.env.DB_USER,
   process.env.DB_PASS,
   {
-    host: "localhost",
+    host: process.env.DB_HOST,
     dialect: "postgres",
+    dialectOptions: {
+      ssl: false,
+    },
     logging: false,
   }
 );
 
 module.exports = sequelize;
+console.log(process.env.DB_USER, process.env.DB_PASS);
