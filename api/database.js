b3b9b074fd1f772d9b33f624ec6f98bfc70778cf
andrii post <postoliuk.av@gmail.com>
2024-05-31 15:42:47 +0300

    database: extended code

--- api/database.js
+++ api/database.js
@@ -1,7 +1,8 @@
 const { Sequelize } = require("sequelize");
 require("dotenv").config();
 
-// const sequelize = new Sequelize(process.env.DATABASE_URL, {  // for railway postgres database
+// const sequelize = new Sequelize(process.env.DATABASE_URL, {
+//   // for railway postgres database
 //   dialect: "postgres",
 //   protocol: "postgres",
 //   dialectOptions: {
@@ -12,30 +13,47 @@ require("dotenv").config();
 //   },
 // });
 
-const sequelize = new Sequelize( // for local fidb database
-  process.env.DB_NAME,
-  process.env.DB_USER,
-  process.env.DB_PASS,
-  {
-    host: process.env.DB_HOST,
-    dialect: "postgres",
-    dialectOptions: {
-      ssl: false,
-    },
-    logging: false,
-  }
-);
+const dbName = process.env.PGDATABASE || "railway";
+const dbUser = process.env.PGUSER || "postgres";
+const dbPass = process.env.PGPASSWORD || "ntbkVWGBsHSKhRGJeKgOfjinLhhSJxqJ";
+const dbHost = process.env.PGHOST || "monorail.proxy.rlwy.net";
+const dbPort = process.env.PGPORT ? parseInt(process.env.PGPORT) : 33230;
+const dbSsl = process.env.DB_SSL === "true";
 
-// sequelize
-//   .query(
-//     "SELECT table_name FROM information_schema.tables WHERE table_schema = 'public';"
-//   )
-//   .then(([results, metadata]) => {
-//     console.log("Tables in the database:", results);
-//   })
-//   .catch((err) => {
-//     console.error("Error fetching tables:", err);
-//   });
+const sequelize = new Sequelize(dbName, dbUser, dbPass, {
+  host: dbHost,
+  port: dbPort,
+  dialect: "postgres",
+  dialectOptions: dbSsl
+    ? {
+        ssl: {
+          require: true,
+          rejectUnauthorized: false,
+        },
+      }
+    : {},
+  logging: false,
+});
+
+// const sequelize = new Sequelize( // for local fidb database
+//   process.env.DB_NAME,
+//   process.env.DB_USER,
+//   process.env.DB_PASS,
+//   {
+//     host: process.env.DB_HOST,
+//     dialect: "postgres",
+//     dialectOptions: {
+//       ssl:
+//         process.env.DB_SSL === "true"
+//           ? {
+//               require: true,
+//               rejectUnauthorized: false,
+//             }
+//           : false,
+//     },
+//     logging: false,
+//   }
+// );
 
 module.exports = sequelize;
 console.log(process.env.DB_USER, process.env.DB_PASS, process.env.DATABASE_URL);
