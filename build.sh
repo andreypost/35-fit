af6ef4aa13184ddd240e7eec72a68645098ee688
andrii post <postoliuk.av@gmail.com>
2024-06-07 15:39:35 +0300

    nest-api.yaml, build.sh edits

--- build.sh
+++ build.sh
@@ -9,4 +9,14 @@ echo "Building Nest API"
 cd ./nest-api
 npm install --frozen-lockfile
 npm run build
+
+echo "Running tests"
+cd ./nest-api
+npm run test:cov
+if [ $? -ne 0 ]; then
+  echo "Tests failed. Exiting..."
+  exit 1
+fi
+
+echo "Starting Nest API in production mode"
 npm run start:prod
