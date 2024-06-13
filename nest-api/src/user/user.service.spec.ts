84dfae0b1448124860473c3102a74e7ba412af38
andrii post <postoliuk.av@gmail.com>
2024-06-13 14:31:08 +0300

    nest-api: added new unit user.service.spec.ts jest test

---
+++ nest-api/src/user/user.service.spec.ts
@@ -0,0 +1,78 @@
+import { TestingModule, Test } from '@nestjs/testing';
+import { getRepositoryToken } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
+import { UserService } from './user.service';
+import { User } from './user.entity';
+import { CreateUserDto } from './dto/create-user.dto';
+import * as bcrypt from 'bcrypt';
+
+const mockUserData = {
+  id: 1,
+  email: 'test@email',
+  password: 'hashedPassword',
+};
+
+const mockUserRepository = () => ({
+  findOne: jest.fn(),
+  create: jest.fn(),
+  save: jest.fn(),
+});
+
+describe('UserService', () => {
+  let service: UserService;
+  let repository: Repository<User>;
+  let result = null;
+
+  beforeEach(async () => {
+    const module: TestingModule = await Test.createTestingModule({
+      providers: [
+        UserService,
+        {
+          provide: getRepositoryToken(User),
+          useFactory: mockUserRepository,
+        },
+      ],
+    }).compile();
+
+    service = module.get<UserService>(UserService);
+    repository = module.get<Repository<User>>(getRepositoryToken(User));
+  });
+
+  describe('findUserByEmail', () => {
+    it('should return a user if found', async () => {
+      (repository.findOne as jest.Mock).mockResolvedValue(mockUserData.email);
+
+      result = await service.findUserByEmail('test@email');
+      expect(result).toEqual(mockUserData.email);
+    });
+
+    it('should return undefined if a user not found', async () => {
+      (repository.findOne as jest.Mock).mockResolvedValue(undefined);
+
+      result = await service.findUserByEmail(undefined);
+      expect(result).toBeUndefined();
+    });
+  });
+
+  describe('createUser', () => {
+    it('should create and return a new user', async () => {
+      const createUserDto: CreateUserDto = {
+        email: mockUserData.email,
+        password: mockUserData.password,
+      };
+      jest.spyOn(bcrypt, 'hash').mockResolvedValue(createUserDto.password);
+      (repository.create as jest.Mock).mockReturnValue(createUserDto);
+      (repository.save as jest.Mock).mockResolvedValue(createUserDto);
+
+      result = await service.createUser(createUserDto);
+
+      expect(result).toEqual(createUserDto);
+
+      expect(bcrypt.hash).toHaveBeenCalledWith(createUserDto.password, 10);
+
+      expect(repository.create).toHaveBeenCalledWith(createUserDto);
+
+      expect(repository.save).toHaveBeenCalledWith(createUserDto);
+    });
+  });
+});
