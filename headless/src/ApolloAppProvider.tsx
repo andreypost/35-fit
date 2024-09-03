595123428811b4d45b92ed0afd06b6484e3c6b4a
andrii post <postoliuk.av@gmail.com>
2024-09-03 13:04:16 +0300

    fixed children type; some .yaml fixes

--- headless/src/ApolloAppProvider.tsx
+++ headless/src/ApolloAppProvider.tsx
@@ -1,11 +1,12 @@
 import { ApolloClient, InMemoryCache, ApolloProvider } from '@apollo/client'
+import { IChildrenTitleDescrip } from 'types/interface'
 
 const client = new ApolloClient({
   uri: `${process.env.API_URL}/graphql`,
   cache: new InMemoryCache(),
 })
 
-const ApolloAppProvider = ({ children }) => (
+const ApolloAppProvider = ({ children }: IChildrenTitleDescrip) => (
   <ApolloProvider client={client}>{children} </ApolloProvider>
 )
 
