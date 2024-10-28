7dc052bfa6df863e478112384f6f63cdbecf3ec3
andrii post <postoliuk.av@gmail.com>
2024-10-28 20:10:23 +0200

    added new nuxt project

---
+++ nuxt-headless/pages/faq.vue
@@ -0,0 +1,9 @@
+<template>
+  <h1>Hello from FAQ page!</h1>
+</template>
+
+<script lang="ts">
+export default {
+  name: 'FaqPage',
+}
+</script>
---
+++ nuxt-headless/pages/index.vue
@@ -0,0 +1,14 @@
+<template>
+  <div>
+    <NuxtLink to="/faq">faq</NuxtLink>
+    <Tutorial />
+  </div>
+</template>
+
+<script lang="ts">
+import Vue from 'vue'
+
+export default Vue.extend({
+  name: 'IndexPage',
+})
+</script>
