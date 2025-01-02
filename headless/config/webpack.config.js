0e597a2fcb59dd5bd57d4a4a061e1522f8a8c3ea
andrii post <postoliuk.av@gmail.com>
2025-01-02 14:44:01 +0100

    added netlify descript

--- headless/config/webpack.config.js
+++ headless/config/webpack.config.js
@@ -4,6 +4,8 @@ const commonConfig = require('./webpack.common.js')
 
 module.exports = (envVars) => {
   const { env, netlify } = envVars
+  // netlify flags can be passed from package.json file with script command:
+  // "netlify": "npx webpack --config config/webpack.config.js --env env=prod netlify=true",
   const envConfig = require(`./webpack.${env}.js`)
   return merge(commonConfig({ netlify }), envConfig)
 }
