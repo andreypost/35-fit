67f57023d2e9a48567f359ba7144bbf7fdef6f01
andrii post <postoliuk.av@gmail.com>
2025-01-02 12:26:06 +0100

    added netlify settings

--- headless/config/webpack.common.js
+++ headless/config/webpack.common.js
@@ -3,84 +3,86 @@ const path = require('path')
 const HtmlWebpackPlugin = require('html-webpack-plugin')
 // const SpriteLoaderPlugin = require('svg-sprite-loader/plugin')
 
-module.exports = {
-  entry: path.resolve(__dirname, '../src/index.tsx'),
-  resolve: {
-    extensions: ['.tsx', '.ts', '.js'],
-    alias: {
-      config: path.resolve(__dirname, '../src/config.ts'),
-      components: path.resolve(__dirname, '../src/components'),
-      HeaderBanner$: path.resolve(
-        __dirname,
-        '../src/components/HeaderBanner.tsx'
-      ),
-      Spinner$: path.resolve(__dirname, '../src/components/Spinner.tsx'),
-      FooterBanner$: path.resolve(
-        __dirname,
-        '../src/components/FooterBanner.tsx'
-      ),
-      // Footer$: path.resolve(__dirname, '../src/components/Footer.tsx'),
-      // Nav$: path.resolve(__dirname, '../src/components/Nav.tsx'),
-      queries: path.resolve(__dirname, '../src/graphql/queries'),
-      modals: path.resolve(__dirname, '../src/modals'),
-      img: path.resolve(__dirname, '../src/img'),
-      svg: path.resolve(__dirname, '../src/img/svg'),
-      mixins$: path.resolve(__dirname, '../src/styles/variables-mixins.scss'),
-      slices: path.resolve(__dirname, '../src/slices'),
-      utils: path.resolve(__dirname, '../src/utils'),
-      views: path.resolve(__dirname, '../src/views'),
-      store$: path.resolve(__dirname, '../src/store/root.store.ts'),
-      reducer$: path.resolve(__dirname, '../src/store/root.reducer.ts'),
-      types: path.resolve(__dirname, '../src/types'),
-      // '@': path.resolve(__dirname, 'src'),
+module.exports = ({ netlify }) => {
+  return {
+    entry: path.resolve(__dirname, '../src/index.tsx'),
+    resolve: {
+      extensions: ['.tsx', '.ts', '.js'],
+      alias: {
+        config: path.resolve(__dirname, '../src/config.ts'),
+        components: path.resolve(__dirname, '../src/components'),
+        HeaderBanner$: path.resolve(
+          __dirname,
+          '../src/components/HeaderBanner.tsx'
+        ),
+        Spinner$: path.resolve(__dirname, '../src/components/Spinner.tsx'),
+        FooterBanner$: path.resolve(
+          __dirname,
+          '../src/components/FooterBanner.tsx'
+        ),
+        // Footer$: path.resolve(__dirname, '../src/components/Footer.tsx'),
+        // Nav$: path.resolve(__dirname, '../src/components/Nav.tsx'),
+        queries: path.resolve(__dirname, '../src/graphql/queries'),
+        modals: path.resolve(__dirname, '../src/modals'),
+        img: path.resolve(__dirname, '../src/img'),
+        svg: path.resolve(__dirname, '../src/img/svg'),
+        mixins$: path.resolve(__dirname, '../src/styles/variables-mixins.scss'),
+        slices: path.resolve(__dirname, '../src/slices'),
+        utils: path.resolve(__dirname, '../src/utils'),
+        views: path.resolve(__dirname, '../src/views'),
+        store$: path.resolve(__dirname, '../src/store/root.store.ts'),
+        reducer$: path.resolve(__dirname, '../src/store/root.reducer.ts'),
+        types: path.resolve(__dirname, '../src/types'),
+        // '@': path.resolve(__dirname, 'src'),
+      },
     },
-  },
-  module: {
-    rules: [
-      {
-        test: /\.(ts|js)x?$/,
-        exclude: /node_modules/,
-        use: [{ loader: 'babel-loader' }],
-      },
-      {
-        test: /\.(?:svg|ico|gif|png|jpg|jpeg|webp)$/i,
-        type: 'asset/resource',
-      },
-      // { // @font-face type of connetion fonts is much more slower.
-      //   test: /\.(woff(2)?|eot|ttf|otf)$/,
-      //   type: 'asset/inline',
-      // },
-      // { // rules for svg to load as inline, and as sprites in index.html and use them in components -> <svg><use xlinkHref={arrow}></use></svg>
-      //   test: /\.svg$/,
-      //   include: [path.resolve(__dirname, '../src/img/inlineSvg')],
-      //   type: 'asset/inline',
-      // },
-      // {
-      //   test: /\.svg$/,
-      //   include: [path.resolve(__dirname, '../src/img/spriteSvg')],
-      //   loader: 'svg-sprite-loader',
-      // },
+    module: {
+      rules: [
+        {
+          test: /\.(ts|js)x?$/,
+          exclude: /node_modules/,
+          use: [{ loader: 'babel-loader' }],
+        },
+        {
+          test: /\.(?:svg|ico|gif|png|jpg|jpeg|webp)$/i,
+          type: 'asset/resource',
+        },
+        // { // @font-face type of connetion fonts is much more slower.
+        //   test: /\.(woff(2)?|eot|ttf|otf)$/,
+        //   type: 'asset/inline',
+        // },
+        // { // rules for svg to load as inline, and as sprites in index.html and use them in components -> <svg><use xlinkHref={arrow}></use></svg>
+        //   test: /\.svg$/,
+        //   include: [path.resolve(__dirname, '../src/img/inlineSvg')],
+        //   type: 'asset/inline',
+        // },
+        // {
+        //   test: /\.svg$/,
+        //   include: [path.resolve(__dirname, '../src/img/spriteSvg')],
+        //   loader: 'svg-sprite-loader',
+        // },
+      ],
+    },
+    plugins: [
+      new HtmlWebpackPlugin({
+        title: '35 fit',
+        template: path.resolve(__dirname, '../public/template.html'),
+        favicon: path.resolve(__dirname, '../public/favicon.ico'),
+        // filename: 'template.html',
+        // cache: false,
+      }),
     ],
-  },
-  plugins: [
-    new HtmlWebpackPlugin({
-      title: '35 fit',
-      template: path.resolve(__dirname, '../public/template.html'),
-      favicon: path.resolve(__dirname, '../public/favicon.ico'),
-      // filename: 'template.html',
-      // cache: false,
-    }),
-  ],
-  output: {
-    clean: true,
-    path: path.resolve(__dirname, '../../build'),
-    filename: '[name].bundle.js',
-    // publicPath: '/',  // makes it impossible to load index.html directly from build folder
-  },
-  stats: 'errors-only',
-  optimization: {
-    splitChunks: {
-      chunks: 'all',
+    output: {
+      clean: true,
+      path: path.resolve(__dirname, netlify ? '../dist' : '../../build'),
+      filename: '[name].bundle.js',
+      // publicPath: '/',  // makes it impossible to load index.html directly from build folder
     },
-  },
+    stats: 'errors-only',
+    optimization: {
+      splitChunks: {
+        chunks: 'all',
+      },
+    },
+  }
 }
--- headless/config/webpack.config.js
+++ headless/config/webpack.config.js
@@ -3,7 +3,7 @@ const { merge } = require('webpack-merge')
 const commonConfig = require('./webpack.common.js')
 
 module.exports = (envVars) => {
-  const { env } = envVars
+  const { env, netlify } = envVars
   const envConfig = require(`./webpack.${env}.js`)
-  return merge(commonConfig, envConfig)
+  return merge(commonConfig({ netlify }), envConfig)
 }
